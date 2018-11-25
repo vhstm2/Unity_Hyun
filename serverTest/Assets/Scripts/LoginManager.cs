fc2ab0808229fbf26965fed3ba9f3a11a216f3d8
Hyun <hyun19899@naver.com>
2018-11-25 15:00:53 +0900

    학원 유니티 서버예제01

---
+++ serverTest/Assets/Scripts/LoginManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Text;
+
+
+public struct SignData      //로그인
+{
+    public string id, pw;
+}
+
+public struct SignUpData    //회원가입
+{
+    public string id, pw , name;
+}
+
+
+
+public class LoginManager : MonoBehaviour {
+
+
+    [SerializeField]
+    private InputField IDField;
+
+    [SerializeField]
+    private InputField PWField;
+
+
+    //로그인
+    public void OnSignInButtonClicked()
+    {
+
+        string id = IDField.text;
+        string pw = PWField.text;
+
+        if (!string.IsNullOrEmpty(id) && !string.IsNullOrEmpty(pw))
+        {
+            StartCoroutine(signin(id,pw));
+        }
+
+
+    }
+
+    public void OnSignUpButtonClicked()
+    {
+
+    }
+
+    IEnumerator signin(string id, string pw)
+    {
+        SignData data = new SignData { id = "goya", pw = "1234" };
+        string jsonData = JsonUtility.ToJson(data);
+        byte[] sendData = Encoding.UTF8.GetBytes(jsonData);
+
+        using (UnityWebRequest www = UnityWebRequest.Put("http://localhost:3000/users/login", sendData))
+        {
+
+            www.method = "POST";
+            www.SetRequestHeader("Content-Type", "application/json");
+
+            Debug.Log(www.downloadHandler.text);
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+
+    //IEnumerator signUp(string id , string pw, string name)
+    //{
+    //    //using (UnityWebRequest www)
+    //}
+
+
+
+
+}
---
+++ serverTest/Assets/Scripts/UIMANAGER.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Text;
+
+
+
+[System.Serializable]
+public class ServerDate
+{
+    public string date;
+}
+
+public class UIMANAGER : MonoBehaviour {
+
+    [SerializeField]
+    private Text timeText;
+
+
+    IEnumerator GetDate()
+    {
+        //웹서버에 요청하는 기능을 담은 클래스
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:3000/date"))
+        {
+            yield return www.SendWebRequest();
+
+            //서버가 오류가 생겻을떄
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);//서버에 전달받은 녀석을 문자열로 받을수있다.?
+                string result = www.downloadHandler.text;
+                ServerDate date = JsonUtility.FromJson<ServerDate>(result);
+                timeText.text = date.date;
+            }
+        }
+    }
+
+
+    #region UI_Events
+    public void OnGetTimeButtonCilker()
+    {
+
+        StartCoroutine(GetDate());
+        
+    }
+    #endregion
+
+
+
+}
